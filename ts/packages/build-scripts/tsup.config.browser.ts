8665ff6e6d57a8b30ba6288fe1184de294bfeb8e
Thorsten Lorenz <thlorenz@gmx.de>
2024-01-23 12:53:33 -0700

    init: shamelessly copy build-scripts from web3.js

---
+++ ts/packages/build-scripts/tsup.config.browser.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'tsup'
+
+import { getBaseConfig } from './getBaseConfig'
+
+export default defineConfig((options) => [
+  ...getBaseConfig('browser', ['cjs', 'esm'], options),
+])
