bc220634e24a30ee3f57ba9ee22af3699c51731d
Thorsten Lorenz <thlorenz@gmx.de>
2024-03-28 18:57:53 -0500

    feat: luzid-grpc with proto-gen script

---
+++ dart/luzid-grpc/lib/luzid_grpc.dart
@@ -0,0 +1,7 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'proto/requests/validator_ops.pb.dart';
+export 'proto/requests/validator_ops.pbgrpc.dart';
